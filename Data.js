929106f61561f4d62426e8de26bd8ba0ef496465
David Ernst <git@dsernst.com>
2015-07-10 01:02:11 -0700

    Data.js code cleanup

--- Data.js
+++ Data.js
@@ -1,32 +1,10 @@
+/*global SpreadsheetApp*/
+
 var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 
-var responsesSheet = spreadsheet.getSheetByName('responses');
-var responses = arraysToObjects(responsesSheet.getDataRange().getValues());
-responses = responses.reduce(function (memo, row) {
-  Object.keys(row).forEach(function (name) {
-    if (!memo.hasOwnProperty(name)) {
-      memo[name] = [];
-    }
-    memo[name].push(row[name]);
-  });
-  return memo;
-}, {});
-for (name in responses) {
-  responses[name] = shuffle(responses[name]);
-}
-
-var contacts = spreadsheet.getSheetByName('contacts')
-                .getDataRange().getValues()
-                .reduce(function (memo, row) {
-                    if (row[1]) {
-                      memo[row[0]] = row[1];
-                    }
-                    return memo;
-                  }, {});
-
-function arraysToObjects(arrayOfArrays) {
-  // This function takes an array of arrays — such as the result of someSheet.getDataRange().getValues() — and transforms it
-  // to an array of objects, where the first item in the array (column headers) are used as property names
+function getSpreadsheetData(sheetName) {
+  // This function gives you an array of objects modeling a worksheet's tabular data, where the first items — column headers — become the property names.
+  var arrayOfArrays = spreadsheet.getSheetByName(sheetName).getDataRange().getValues();
   var headers = arrayOfArrays.shift();
   return arrayOfArrays.map(function (row) {
     return row.reduce(function (memo, value, index) {
@@ -38,8 +16,29 @@ function arraysToObjects(arrayOfArrays) {
   });
 }
 
-function shuffle(o){
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+function shuffle(o) {
+  for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
 }
 
+var responses = getSpreadsheetData('responses').reduce(function (memo, row) {
+  Object.keys(row).forEach(function (name) {
+    if (!memo.hasOwnProperty(name)) {
+      memo[name] = [];
+    }
+    memo[name].push(row[name]);
+  });
+  return memo;
+}, {});
+for (var name in responses) {
+  responses[name] = shuffle(responses[name]);
+}
+
+var contacts = spreadsheet.getSheetByName('contacts')
+                .getDataRange().getValues()
+                .reduce(function (memo, row) {
+                    if (row[1]) {
+                      memo[row[0]] = row[1];
+                    }
+                    return memo;
+                  }, {});
